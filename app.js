78e1e5fab5b09373852d781b6feb73b5775efe14
GabriOliveira <gabriel007og@gmail.com>
2024-09-06 21:01:00 -0300

    Aula 05 Finalizada, ajustes em funções de busca

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 function pesquisar() {
-    const section = document.getElementById("resultados-pesquisa");
-    const input = document.getElementById("input-pesquisa").value.toLowerCase(); // Obtém o valor da pesquisa e converte para minúsculas
-  
-    let resultados = "";
-  
-    // Itera sobre cada dado e filtra os resultados
-    for (let dado of dados) {
-      if (dado.titulo.toLowerCase().includes(input)) {
-        resultados += `
+  const section = document.getElementById("resultados-pesquisa");
+  const campoPesquisa = document
+    .getElementById("input-pesquisa")
+    .value.toLowerCase(); // Obtém o valor da pesquisa e converte para minúsculas
+
+  if (campoPesquisa == "" || " ") {
+    section.innerHTML = "<h3>Nada Foi Encontrado :(</h3>";
+    return;
+  }
+
+  let resultados = "";
+  // Itera sobre cada dado e filtra os resultados
+  for (let dado of dados) {
+    if (
+      dado.titulo.toLowerCase().startsWith(campoPesquisa) ||
+      dado.descricao.toLowerCase().includes(campoPesquisa) ||
+      dado.tags.toLowerCase().includes(campoPesquisa)
+    ) {
+      resultados += `
           <div class="item-resultado">
             <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
             <p class="descricao-meta">${dado.descricao}</p>
             <a href="${dado.link}" target="_blank">Saiba mais</a>
           </div>
         `;
-      }
     }
-  
-    section.innerHTML = resultados;
   }
+  if(!resultados){
+    resultados = '<p>Nada foi encontrado</p>';
+  }
+
+  section.innerHTML = resultados;
+}
